a820dbaf5a604ffcf1029af72d0925deba72dd35
d4n <dan3436@hotmail.com>
2020-12-05 15:06:52 -0500

    Add day 5 solution.

---
+++ src/Days/Day5.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode2020.Days
+{
+    public class Day5 : BaseDay<string, int>
+    {
+        public override int Day => 5;
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Input))]
+        public override int Part1(string[] input)
+        {
+            int max = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int id = DecodeSeat(input[i]);
+                if (id > max)
+                    max = id;
+            }
+
+            return max;
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Input))]
+        public override int Part2(string[] input)
+        {
+            int sum = 0;
+            int min = input.Length;
+            int max = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int id = DecodeSeat(input[i]);
+                if (id > max)
+                    max = id;
+                if (id < min)
+                    min = id;
+                sum += id;
+            }
+
+            int seat = ((max + min) * (max - min + 1) / 2) - sum;
+
+            return seat;
+        }
+
+        private static int DecodeSeat(string seat)
+        {
+            return Convert.ToInt32(seat
+                .Replace('L', '0')
+                .Replace('B', '1')
+                .Replace('F', '0')
+                .Replace('R', '1'), 2);
+        }
+    }
+}
