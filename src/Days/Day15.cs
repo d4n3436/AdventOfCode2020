442ec128cfe2205d4c351dce6c30e5327330a305
d4n <dan3436@hotmail.com>
2020-12-16 02:41:10 -0500

    Day 15

---
+++ src/Days/Day15.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode2020.Days
+{
+    [MemoryDiagnoser]
+    public class Day15 : BaseDay<int>
+    {
+        public override int Day => 15;
+
+        [Benchmark]
+        public override int Part1()
+            => GetNumberSpokenAt(2020 - 1, Array.ConvertAll(Input.Split(','), int.Parse));
+
+        [Benchmark]
+        public override int Part2()
+            => GetNumberSpokenAt(30000000 - 1, Array.ConvertAll(Input.Split(','), int.Parse));
+
+        private static int GetNumberSpokenAt(int index, int[] initial)
+        {
+            int[] turns = new int[index + 1];
+            var dict = new Dictionary<int, (int, int)>();
+
+            for (int i = 0; i < turns.Length; i++)
+            {
+                if (i < initial.Length)
+                    turns[i] = initial[i];
+                else if (dict.TryGetValue(turns[i - 1], out var last) && last.Item1 != -1 && last.Item2 != -1)
+                    turns[i] = last.Item1 - last.Item2;
+
+                if (dict.TryGetValue(turns[i], out var current))
+                    dict[turns[i]] = (i, current.Item1);
+                else
+                    dict[turns[i]] = (i, -1);
+            }
+
+            return turns[index];
+        }
+    }
+}
